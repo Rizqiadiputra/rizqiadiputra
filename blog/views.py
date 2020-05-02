0430e8caf59323b38b2661993548183a029e0699
rizqi adiputra <rizqiadiputra1996@gmail.com>
2020-05-02 15:14:25 +0700

    connect model to template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def daftar_post(request):
-    return render(request, 'blog/daftar_post.html',{})
+    posts_diobjectview = Post.objects.filter(tgl_terbit__lte=timezone.now()).order_by('tgl_buat')
+    # print(posts_diobjectview)
+    #{} = tempat dimana kita dapat menambahkan sesuatu untuk digunakan di template
+    return render(request, 'blog/daftar_post.html', {'posts_ditemplate':posts_diobjectview})
