ec3425bf5c8f05ed46034eea01765cf9af697ad5
rizqi adiputra <rizqiadiputra1996@gmail.com>
2020-05-02 16:32:51 +0700

    add static file and base html

--- rizqiadiputra/settings.py
+++ rizqiadiputra/settings.py
@@ -120,5 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
 #static root = path for static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
