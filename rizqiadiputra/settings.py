a3e18a9c3e0afbb285e2f657da592fdee8a455f2
rizqi adiputra <rizqiadiputra1996@gmail.com>
2020-05-03 06:12:59 +0700

    edit debug to false

--- rizqiadiputra/settings.py
+++ rizqiadiputra/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9e0#5%2^@m*f(1_jd=f2rn93^%u0b+$g(_s497*+ktm(8ggb=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 #host name
 ALLOWED_HOSTS = ['127.0.0.1','172.105.126.84']
