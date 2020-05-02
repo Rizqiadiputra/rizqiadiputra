e41c989d16276527e77237fe68e6ed2cacc7a03a
rizqi adiputra <rizqiadiputra1996@gmail.com>
2020-05-03 06:38:06 +0700

    change db to postgresql

--- rizqiadiputra/settings.py
+++ rizqiadiputra/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'rizqiadiputra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'rizqiadiputra_prod'),
+        'USER': root,
+        'PASSWORD': 'Root_12345'
+        'HOST': 'localhost'
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
