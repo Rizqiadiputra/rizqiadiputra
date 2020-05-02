0b8dbab2dcbe08a531fc32278d4048b4960affb4
rizqi adiputra <rizqiadiputra1996@gmail.com>
2020-05-02 16:54:16 +0700

    add detail post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.daftar_post, name='daftar_post'),
+    path('post/<int:pk>/', views.detail_post, name='detail_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -8,3 +8,9 @@ def daftar_post(request):
     # print(posts_diobjectview)
     #{} = tempat dimana kita dapat menambahkan sesuatu untuk digunakan di template
     return render(request, 'blog/daftar_post.html', {'posts_ditemplate':posts_diobjectview})
+
+#pk = primary key = unique identifier for each record in a database
+#django secara default membuat id per data sebagai pk
+def detail_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail_post.html', {'post':post})
